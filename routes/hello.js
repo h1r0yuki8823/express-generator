ee2cd3d136a41b62a4175055f535483b8544ca2c
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-05-04 15:46:50 +0900

    use generator

---
+++ routes/hello.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',(req, res, next) => {
+    var name = req.query.neme;
+    var mail = req.query.mail;
+    var data = {
+        title: 'Hello!',
+        content: '※何か書いて送信してください。'
+    };
+    res.render('hello', data);
+});
+
+router.post('/post',(req, res, next) => {
+    var msg = req.body['message'];
+    var data = {
+        title: 'Hello!',
+        content: 'あなたは、「' + msg + '」と送信しました。'
+    };
+    res.render('hello', data);
+});
+
+module.exports = router;
